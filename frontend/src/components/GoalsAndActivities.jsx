8739f68508a2a5fa569a291c1ded587abd604c9d
Chadmcleod3 <chadmcleod3@gmail.com>
2026-03-26 17:52:02 -0400

    Update GoalsAndActivities.jsx

--- frontend/src/components/GoalsAndActivities.jsx
+++ frontend/src/components/GoalsAndActivities.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const API_URL = 'http://192.168.1.90:8080';
+const API_URL = 'http://localhost:8080';
 
 function GoalsAndActivities({ user, communityId, communityName }) {
   const [goals, setGoals] = useState([]);
